3f3db079a80df359ddc0dcdd1f60eed475707928
Astromin21 <docgit@gmail.com>
2023-06-22 10:59:59 +0200

    end conditions added

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 "use strict"
 //-----------------------------Spieler erstellen---------------------------------------------------
 function Players(name, punkte, mark) {
-    this.punkte = 0;
-    this.mark = mark;
-    return {name, punkte, mark}
+    //this.punkte = 0;
+    //this.mark = mark;
+    return {name}
 }
 const init = (() => {
     let startBtn = document.getElementById('start')
@@ -14,17 +14,18 @@ const init = (() => {
             alert('Namen eingeben!');
         } else {
             player1 = new Players(player1, 0, 'X');
-            player2 = new Players(player2, 0, 'O');
+             player2 = new Players(player2, 0, 'O');
             spielen(player1, player2, 'p1');
         }
     })
 })()
 //--------------------------Spiel starten-----------------------------------------------------
 class NeuesSpiel{   
-    constructor(runde, werdran){
+    constructor(runde, werdran, spielfeld){
         this.runde = runde;
         this.werDran = werdran;
-        this.auswahl = ['z','z','z','z','z','z','z','z','z'];
+        this.spielfeld = spielfeld;
+        this.end = 'run';
     }
     get welcheRunde() {
         return this.runde
@@ -39,26 +40,31 @@ class NeuesSpiel{
         this.auswahl = auswahl;
     }
 }
-
-
 //--------------------------Spiel Daten/ Gamemaster func ------------------------------------------------------
 const spielen = (player1, player2, wer) => {
-    let spiel = new NeuesSpiel(0, wer);
+    let spielfeld = ['a','b','c','d','e','f','g','h','i'];
+    let spiel = new NeuesSpiel(0, wer, spielfeld);
     feldErkennen('click', spiel);
     //restart();
 }
 const feldErkennen = (event, spiel) =>{
-    let feld = document.getElementById('spielfeld')
-    feld.addEventListener(event, (e) => {
-        let targetElement = e.target;
-        feldMarkieren(targetElement, spiel);
-        feldErstellen(targetElement, spiel);
-        //playerTurn(spiel);
-        /*
-        while (targetElement != null) {
-            targetElement = targetElement.parentElement;
-        }*/
-    }, true)
+    let feld = document.getElementById('spielfeld')   
+        feld.addEventListener(event, (e) => {
+            let targetElement = e.target;
+            if (spiel.end === 'run'){
+                feldMarkieren(targetElement, spiel);
+            } else if (spiel.end === 'win'){
+                console.log('spiel sieg');
+            } else if (spiel.end === 'draw') {
+                console.log(('spiel draw'));
+            }
+            //playerTurn(spiel);
+            /*
+            while (targetElement != null) {
+                targetElement = targetElement.parentElement;
+            }*/
+        }, true)
+    
 }
 const feldMarkieren = (targetElement, spiel) => {
     if(targetElement.innerHTML === ""){
@@ -72,7 +78,12 @@ const feldMarkieren = (targetElement, spiel) => {
             spiel.werZug = 'p1';
             changeRound(spiel)
         }
-    } else {
+        feldErstellen(targetElement, spiel);
+    }
+    else if(targetElement.innerHTML === 'Restart'){
+        restart()
+    }
+    else {
         alert('Feld ist schon besetzt');
     }
 }
@@ -83,46 +94,30 @@ const changeRound = (spiel) => {
 const feldErstellen = (targetElement, spiel) => {
     let feld = document.querySelector('.felder');
     feld = targetElement.dataset.value;
-    console.log('feld', feld);
-    //spiel.werFeld = spiel.werfeld[feld]
+    spiel.spielfeld[feld] = spiel.werDran;
+    endCondition(spiel);   
 }
-
-/*
 const restart = () => {
-    let btn = document.getElementById('restart')
-    btn.addEventListener('click', () => {
-        init()
-    })
-    console.log('test', player1);
+    navigator.reload();
 }
+const endCondition = (spiel) => { 
+    if( (spiel.spielfeld[0] === spiel.spielfeld[1] && spiel.spielfeld[1] === spiel.spielfeld[2]) ||
+        (spiel.spielfeld[3] === spiel.spielfeld[4] && spiel.spielfeld[4] === spiel.spielfeld[5]) ||
+        (spiel.spielfeld[6] === spiel.spielfeld[7] && spiel.spielfeld[7] === spiel.spielfeld[8]) ||
 
+        (spiel.spielfeld[0] === spiel.spielfeld[3] && spiel.spielfeld[3] === spiel.spielfeld[6]) ||
+        (spiel.spielfeld[1] === spiel.spielfeld[4] && spiel.spielfeld[4] === spiel.spielfeld[7]) ||
+        (spiel.spielfeld[2] === spiel.spielfeld[5] && spiel.spielfeld[5] === spiel.spielfeld[8]) ||
 
-const winCondition = (spiel) => {    
-    if( (0 === 1 && 1 === 2) ||
-        (3 === 4 && 4 === 5)) ||
-        (6 === 7 && 7 === 8) ||
-
-        (0 === 3 && 3 === 6) ||
-        (1 === 4 && 4 === 7) ||
-        (2 === 5 && 5 === 8) ||
-
-        (0 === 4 && 4 === 8) ||
-        (6=== 4 && 4 === 2)     {
-
+        (spiel.spielfeld[0] === spiel.spielfeld[4] && spiel.spielfeld[4] === spiel.spielfeld[8]) ||
+        (spiel.spielfeld[6] === spiel.spielfeld[4] && spiel.spielfeld[4] === spiel.spielfeld[2])){   
+        console.log(spiel.werDran, 'hat gewonnen');
+        spiel.end = 'win';
+    } else if(spiel.welcheRunde >= 8) {
+        spiel.end = 'draw';
     }
-    456
-    789
-
-    741
-    258
-    963
-
-    159
-    753
-
 }
-
-
+/*
 const playerTurn = (spiel) => {
     if(spiel.runde % 2 === 0) {
         spiel.werdran = 'p1';
